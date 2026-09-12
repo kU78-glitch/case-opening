d4059bfba3bc891cc84a736858c541755c7742b7
carbonz <karl.uiga78@gmail.com>
2026-09-13 00:57:25 +0300

    .

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+import sys
+import subprocess
+
+
+def auto_install_dependencies():
+    required_packages = ["customtkinter", "pygame"]
+    for package in required_packages:
+        try:
+            __import__(package)
+        except ImportError:
+            print(f"Package '{package}' missing. Installing automatically...")
+            subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+
+auto_install_dependencies()
+
 from ui.app import App
 
 if __name__ == "__main__":
